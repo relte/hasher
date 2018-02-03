cff9093b3e9edbaf7640a92636dac8680f2de845
relte <9462146+relte@users.noreply.github.com>
2018-02-03 14:21:18 +0100

    Check clipboard if it matches the generated hash

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from tkinter.ttk import *
 from path_resolver import *
 from hash_generator import hash_file
 
-def generate_checksum() -> None:
+def generate_hash() -> None:
     hash.set(hash_file(get_chosen_file_path(), selected_algorithm.get()))
+    check_clipboard_match()
+
+def check_clipboard_match() -> None:
+    is_hash_the_same = hash.get() == root.clipboard_get()
+    is_in_clipboard.set('The hash matches clipboard!' if is_hash_the_same else 'The hash does not match clipboard!')
 
 root = Tk()
 root.title('Hasher')
@@ -16,9 +21,16 @@ selected_algorithm = StringVar()
 algorithms_option_menu = OptionMenu(root, selected_algorithm, ALGORITHMS[0], *ALGORITHMS)
 
 hash = StringVar()
-hash_label = Label(root, textvariable=hash).pack()
+hash_entry = Entry(root, state='readonly', textvariable=hash, width=60).pack()
+
 algorithms_option_menu.pack()
 
-Button(root, text='Generate checksum', command=generate_checksum).pack()
+is_in_clipboard = StringVar()
+is_in_clipboard_label = Label(root, textvariable=is_in_clipboard).pack()
+
+Button(root, text='Generate checksum', command=generate_hash).pack()
+
+generate_hash()
+check_clipboard_match()
 
 root.mainloop()
