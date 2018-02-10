54216dc2c1aeba5d0448daeb66656b4c45777696
relte <9462146+relte@users.noreply.github.com>
2018-02-10 21:41:00 +0100

    Keep config only in config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import path_resolver
-import json
-import os
-
-with open(os.path.join(path_resolver.get_script_dir(), 'config.json')) as file:
-    config = json.load(file)
+config = {
+    'environment': 'dev',
+    'name': 'Hasher'
+}
 
 def is_dev_environment():
     return config['environment'] == 'dev'
